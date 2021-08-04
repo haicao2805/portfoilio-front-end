9b28fc22dc26da2cbb6e17831416794bac5c52f6
hetay566 <heaty566@gmail.com>
2021-08-04 10:12:40 +0700

    First commit

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,53 +1,77 @@
 import * as React from 'react';
 import Link from 'next/link';
-import LogoIcon from '../public/asset/icons/logo';
+import InLogo from '../public/asset/icons/in';
+import GmailLogo from '../public/asset/icons/gmail';
+import FacebookLogo from '../public/asset/icons/facebook';
+import GithubLogo from '../public/asset/icons/github';
 
 export interface NavbarProps {}
 
 const Navbar: React.FunctionComponent<NavbarProps> = () => {
-    const [click, setClick] = React.useState(false);
-
+    const [isActive, setActive] = React.useState(false);
     return (
-        <div className="fixed z-50 w-full p-4 bg-white shadow-lg">
-            <div className="flex items-center justify-between mb-2 lg:px-8 lg:items-end">
-                <div className="items-end space-x-4 lg:flex space">
-                    <div className="flex space-x-2">
-                        <LogoIcon />
-                        <Link href="/">
-                            <h1 className="text-2xl font-bold">THAI THINH</h1>
-                        </Link>
-                    </div>
-
-                    <p className="font-medium tracking-wide lg:font-normal">
-                        <span className="hidden mr-2 lg:inline-block">/</span>
-                        BACKEND DEVELOPER
-                    </p>
+        <div className="fixed z-50 w-full p-4 text-white md:p-8 ">
+            <div className="relative z-30 flex items-center justify-between">
+                <div className="font-serif text-2xl italic font-medium">
+                    <Link href="/">Hai Cao</Link>
                 </div>
-                <ul className="hidden space-x-4 lg:flex">
-                    <li className="font-bold duration-300 lg:font-semibold hover:text-blue-600">ABOUT ME</li>
-                    <li className="font-bold duration-300 lg:font-semibold hover:text-blue-600">RESUME</li>
-                    <li className="font-bold duration-300 lg:font-semibold hover:text-blue-600">PROJECTS</li>
-                    <li className="font-bold duration-300 lg:font-semibold hover:text-blue-600">CONTACT</li>
-                </ul>
-                <div className="flex flex-col space-y-2 lg:hidden" onClick={() => setClick(!click)}>
-                    <div className={`w-4 h-1 bg-blue-600 rounded-lg transform duration-300 ${click && 'translate-x-4'}`}></div>
-                    <div className="w-8 h-1 bg-blue-600 rounded-lg"></div>
-
-                    <div className={`self-end w-4 h-1 bg-blue-600 rounded-lg transform duration-300 ${click && '-translate-x-4'} `}></div>
-                </div>
+                <button
+                    className="menu focus:outline-none"
+                    onClick={(click) => {
+                        setActive(!isActive);
+                        click.currentTarget.classList.toggle('opened');
+                    }}
+                    aria-label="Main Menu"
+                >
+                    <svg width="48" height="48" viewBox="0 0 100 100">
+                        <path
+                            className="line line1"
+                            d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+                        />
+                        <path className="line line2" d="M 20,50 H 80" />
+                        <path
+                            className="line line3"
+                            d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+                        />
+                    </svg>
+                </button>
             </div>
-            <button
-                className={`fixed left-0 w-full h-full grid place-items-center bg-dawn-pink-500 transform duration-300 cursor-pointer z-50 lg:hidden ${
-                    click ? 'translate-x-0' : 'translate-x-full'
-                }`}
+            <div
+                className={`duration-300 origin-top transform  ${
+                    isActive ? 'scale-y-100' : 'scale-y-0'
+                }   bg-cod-gray-500 h-full fixed top-0 left-0 w-full z-20 bg-opacity-70`}
             >
-                <ul className="space-y-8 text-center transform -translate-y-16 ">
-                    <li className="text-4xl font-bold duration-300 hover:text-blue-600">ABOUT ME</li>
-                    <li className="text-4xl font-bold duration-300 hover:text-blue-600">RESUME</li>
-                    <li className="text-4xl font-bold duration-300 hover:text-blue-600">PROJECTS</li>
-                    <li className="text-4xl font-bold duration-300 hover:text-blue-600">CONTACT</li>
+                <ul
+                    className={`flex flex-col items-center justify-center h-full space-y-12 text-3xl transform opacity-0 ${
+                        isActive ? 'fade-in' : ''
+                    }`}
+                >
+                    <li className="duration-300 transform cursor-pointer hover:scale-110 hover:text-yellow-500">HOME</li>
+                    <li className="duration-300 transform cursor-pointer hover:scale-110 hover:text-yellow-500">PORTFOLIO</li>
+                    <li className="duration-300 transform cursor-pointer hover:scale-110 hover:text-yellow-500">BLOG</li>
+                    <li className="duration-300 transform cursor-pointer hover:scale-110 hover:text-yellow-500">ABOUT</li>
+                    <li className="duration-300 transform cursor-pointer hover:scale-110 hover:text-yellow-500">CONTACT</li>
+                    <li>
+                        <div className="flex space-x-4">
+                            <a
+                                href="https://www.linkedin.com/in/cao-ch%C3%AD-h%E1%BA%A3i-127033219/?fbclid=IwAR2MRUuXuliROnVhJW_yaXSGkFe7nEvv9hDSHfK8OfnQrmlgPj_odj5saJc"
+                                target="__blank"
+                            >
+                                <InLogo />
+                            </a>
+                            <a href="mailTo:haicao2805@gmail.com">
+                                <GmailLogo />
+                            </a>
+                            <a href="https://www.facebook.com/profile.php?id=100009261397293" target="__blank">
+                                <FacebookLogo />
+                            </a>
+                            <a href="https://github.com/haicao2805" target="__blank">
+                                <GithubLogo />
+                            </a>
+                        </div>
+                    </li>
                 </ul>
-            </button>
+            </div>
         </div>
     );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import '../styles/tailwind.css';
 import { store } from '../store';
 import Navbar from '../components/navbar';
-import Footer from '../components/footer';
 
 export interface AppProps {
     Component: React.FunctionComponent;
@@ -15,10 +14,26 @@ export interface AppProps {
 const App: React.FunctionComponent<AppProps> = ({ Component, pageProps }) => {
     return (
         <Provider store={store}>
-            <div className="flex flex-col min-h-screen">
+            <div className="relative flex flex-col min-h-screen overflow-hidden bg-container">
+                <video
+                    playsInline
+                    autoPlay
+                    muted
+                    loop
+                    className="absolute top-0 left-0 z-0 object-cover w-full h-full"
+                    poster="https://steamcommunity.com/economy/profilebackground/items/628750/2ea2b54fa28ead1625e272ab0628967ade369c15.jpg?size=320x200"
+                >
+                    <source
+                        src="https://cdn.akamai.steamstatic.com/steamcommunity/public/images/items/628750/ddaba24d87fe20c5e5967e22bcff67e497becd69.webm"
+                        type="video/webm"
+                    />
+                    <source
+                        src="https://cdn.akamai.steamstatic.com/steamcommunity/public/images/items/628750/525c9bbf9c0551a2a5bdb27c8dabb4de2f2b0d5c.mp4"
+                        type="video/mp4"
+                    />
+                </video>
                 <Navbar />
                 <Component {...pageProps} />
-                <Footer />
             </div>
         </Provider>
     );
